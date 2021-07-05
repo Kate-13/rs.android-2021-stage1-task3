b3f10acf9fcabbd0d61b28c82a820536c8f92d56
Kate-13 <kate_boyko@mail.ru>
2021-07-05 15:09:13 +0300

    feat: implement Combinator

--- src/main/kotlin/subtask1/Combinator.kt
+++ src/main/kotlin/subtask1/Combinator.kt
@@ -2,8 +2,31 @@ package subtask1
 
 class Combinator {
 
-    // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+
+        val combinations = arrayListOf<Int>()
+
+        for (k in 1..array[1]){
+            val f = factorial(array[1])/(factorial(k)*factorial(array[1]-k))
+            if ( f == array[0].toDouble()) {
+                combinations.add(k)
+            }
+        }
+        if (combinations.isNotEmpty()) {
+            val x = combinations.min()
+            return x
+        } else {
+            return null
+        }
     }
 }
+
+fun factorial(n: Int): Double {
+    var result = 1.0
+    for (i in 1..n) {
+        result = result * i
+    }
+    return result
+
+}
+
